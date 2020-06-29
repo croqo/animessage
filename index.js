92faf1ea71ad1720691c43a45a349610a0115e6e
Eugene Croquette <eu.gene@outlook.com>
2020-06-29 08:19:18 +0300

    Actual logic

--- index.js
+++ index.js
@@ -1,43 +1,66 @@
-
+let Url = getUrl();
 let Config = getConfig();
 let Play = getLotties();
+let Sound = getSound();
+soundLoad();
 
-$("input, select").on("change", function (event) {
-    let input = event.target;
-    let id = $(input).closest("form").attr("data-target");
-    let name = $(input).attr("name");
+$("input, select, textarea").on("change", function (event) {
+    let id = getFormTargetId(
+        $(this).closest("form").get(0)
+    );
+    let name = $(this).attr("name");
+    let value;
     switch (name) {
         case "loop":
-            Config[id]["loop"] = $(input).prop("checked");
+            value = $(this).prop("checked");
             break;
-
         default:
-            Config[id][name] = $(input).val();
+            value = $(this).val();
     }
+    Config[id][name] = value;
+    Url.searchParams.set(`${id}_${name}`, value);
+    window.history.replaceState(
+        `${id}_${name}: ${value}`,
+        "woohoo!",
+        `${Url.pathname}?${Url.searchParams}`
+    );
 });
 $("form button").on("click", function (event) {
     event.preventDefault();
-    let id = $(this).closest("form").attr("data-target");
+    let id = getFormTargetId(
+        $(this).closest("form").get(0)
+    );
     setCongig(id);
     playLottie(id);
 });
 $("#config").on("click", function (event) {
     $(this).toggleClass("is-warning");
-    $(".hero-foot .container").slideToggle(200);
-    $("#self-config").slideToggle();
+    $("#config-panel").slideToggle(200);
     $(".modal").fadeToggle(200);
+    $(".modal-card").fadeToggle(200);
     $(".lottie-player").each(function () {
         $(this).fadeToggle(200);
     });
 });
 
 function startShow() {
+    $('#config').fadeOut(200);
+    Sound.play();
+    $("#congratulation-text").text(Config["composition"]["text"]);
     $('.modal').fadeIn(200);
     playLottie("background");
-    playLottie("foreground");
-    let t = Config["self"].wait;
+    let d = Config["foreground"]["delay"];
+    setTimeout(function () {
+        playLottie("foreground");
+        $('.modal-card').fadeIn(200);
+    }, d );
+    let t = Config["composition"]["duration"];
     setTimeout(function () {
         $('.modal').fadeOut(200);
+        $('.modal-card').fadeOut(200);
+        Sound.pause();
+        Sound.currentTime = 0;
+        $('#config').fadeIn(200);
     }, t );
 }
 function resetLottie(id) {
@@ -46,11 +69,8 @@ function resetLottie(id) {
 }
 function playLottie(id) {
     resetLottie(id);
-    let delay = Config[id].delay | 0;
-    setTimeout(function () {
-        $("#"+id).fadeIn(100);
-        Play[id].play();
-    }, delay );
+    $("#"+id).fadeIn(100);
+    Play[id].goToAndPlay(0);
 }
 
 function getLotties() {
@@ -69,7 +89,7 @@ function getLottie(id) {
     let result = lottie.loadAnimation({
         container: container,
         renderer: 'svg',
-        loop: config.loop, //!=1?false:true,
+        loop: !!config.loop,
         autoplay: false,
         path: config.url,
         rendererSettings: {
@@ -87,20 +107,56 @@ function setCongig(targetId) {
 
 function getConfig() {
     let result = [];
+    setFormsFromUrl(Url.searchParams);
     $("form").each(function (index) {
         let form = $("form").get(index);
-        let id = form.getAttribute('data-target');
+        let id = getFormTargetId(form);
         result[id] = getForm(form);
     });
     return result;
 }
+function getFormTargetId(form) {
+    return form.getAttribute('data-target');
+}
 function getForm(form) {
     let result = {};
     let data = $(form).serializeArray();
-    data.forEach(function(item, index) {
-        if (item["name"]){
-            result[item["name"]] = item["value"];
+    data.forEach(function(item) {
+        let value;
+        switch (item["name"]) {
+            case "loop":
+                value = item["value"] === "on";
+                break;
+            default:
+                value = item["value"];
         }
+        result[item["name"]] = value;
     });
     return result;
+}
+function getSound() {
+    let s = $("#sound");
+    return s[0];
+}
+function soundLoad() {
+    $("#mp3").attr("src", Config["composition"]["sound"]);
+    Sound.pause();
+    Sound.load();
+}
+function getUrl(){
+    let href = window.location.href;
+    return new URL(href);
+}
+function setFormsFromUrl(searchParams){
+    for(let [name, value] of searchParams) {
+        let a = name.split("_");
+        let selector = `form[data-target="${a[0]}"]`;
+        switch (a[1]) {
+            case 'loop':
+                $(selector+' [name="loop"]').attr('checked',value!=="false");
+                break;
+            default:
+                $(`${selector} [name="${a[1]}"]`).val(value);
+        }
+    }
 }
