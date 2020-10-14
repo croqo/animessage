cb42b3fe24937fdc44ab312d9b192891e2e8582b
Eugene Croquette <croqo@outlook.com>
2020-10-14 03:26:47 +0300

    CU-90xj5z universal constructor

--- src/js/lottie.js
+++ src/js/lottie.js
@@ -2,39 +2,80 @@
 
 import Core from "./core-jquery";
 import $ from "jquery";
+import lottie from "lottie-web";
 
 let defaults = {
     id: 'none',
-    path: 'x',
+    path: 'none',
     auto: true,
     loop: false,
-    speed: 1.01
+    speed: 1.01,
+    container: $('figure.lottie-player')
 }
-
-
+'use strict'
 export default class Lottie
 {
-    constructor(Obj=Object) {
-        this.id = (!!Obj.id) ?  Obj.id : defaults.id;
-        this.path = (!!Obj.path) ? Obj.path : defaults.path;
-        this.auto = (!!Obj.auto) ? Obj.auto : defaults.auto;
-        this.loop = (!!Obj.loop) ? Obj.loop : defaults.loop;
-        this.speed = (!!Obj.speed) ? Obj.speed : defaults.speed;
+    constructor(Obj=defaults) {
+        for (const [key, value] of Object.entries(Obj))
+        {
+            Object.defineProperty(
+                this,
+                key,
+                {
+                    value: value,
+                    writable: true,
+                    enumerable: true
+                });
+        }
+        console.log(this);
     }
+
     static async getEmAll() {
         let result = [];
         Core.getConfig().then(function (config)
             {
                 $('.lottie-player').each(function(index){
-                    let player = $('.lottie-player').get(index);
-                    let id = player.getAttribute('id');
-                    let c = config[id];
+                    // console.log(this);
+                    let player = this,
+                        id = player.id,
+                        c = config[id];
+
                     c.id = id;
+                    c.container = player;
                     result[id] = new Lottie(c);
                 });
             }
         );
         return result;
     }
-
+    static async getFromContainer(container)
+    {
+        let id = container.id;
+        let c = Core.getConfig().then(function (cfg)
+        {
+            let r = new Lottie(c);
+                r.id = id;
+                r.container = container;
+            return r
+        });
+    }
+    static async build(model)
+    {
+        let r = lottie.loadAnimation({
+            path: model.path | false,
+            container: model.container,
+            renderer: 'svg',
+            loop: !!model.loop,
+            autoplay: !!model.auto,
+            rendererSettings: {
+                progressiveLoad: true,
+                preserveAspectRatio: 'xMidYMid slice'
+            }
+        });
+        $(model.container).on('DOMLoaded', function ()
+        {
+            r.setSpeed(model.speed);
+        });
+        return r;
+    }
 }
