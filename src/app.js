28b862658332d2cb12e40aa9fe577d03e20f520e
Eugene Croquette <croqo@outlook.com>
2020-10-14 05:12:45 +0300

    CU-90xj5z main script working checkpoint

--- src/app.js
+++ src/app.js
@@ -5,27 +5,31 @@ import lottie from 'lottie-web';
 window.lottie = lottie;
 
 
-let Sound;
-let Animation;
+// let Sound;
 window.a = {};
 import Core from './js/core-jquery';
 import Reactor from './js/reactor-jquery';
 window.a.Reactor = Reactor;
 import Route from './js/route';
-import Clipboard from './js/clipboard';
+// import Animation from './js/animation';
+import Lottie from './js/lottie';
+// import Clipboard from './js/clipboard';
 
 $(document).ready(function () {
-    let Url = Route.new();
-    let c = new Core();
-    setTimeout(function ()
-    {
-        console.log(c);
-    },1000);
+    // let Url = Route.new();
+    // let c = new Core();
+    // console.log(c);
+    Lottie.getEmAll().then(function (res){
+        // console.log(res);
+        Object.keys(res).forEach(function ($key, index, array)
+        {
+            let model = new Lottie(res[$key]);
+            console.log(model);
 
-
-    // if (Url.search!==""){
-    //   a.Route.set(`#${btoa(Url.search)}`);
-    // }
-
-    // init();
+            Lottie.build(model).then(function (res)
+            {
+                console.log(res);
+            });
+        });
+    })
 });
