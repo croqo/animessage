694a83fb9407cac12a884d7ef9247b892ccaf73e
Eugene Croquette <croqo@outlook.com>
2020-10-16 09:35:50 +0300

    CU-90xj5z Constructor form model cleanup.

--- src/js/form-jquery.js
+++ src/js/form-jquery.js
@@ -1,9 +1,26 @@
 import Model from "./model";
+import $ from "jquery";
 
 export default class Form extends Model
 {
-    set selector(id)
-    {
-        return `form[data-target="${id}"]`;
+    static default(){return 'form'};
+
+    constructor(form) {
+        let data = Object.assign(
+            $(form).data(),
+            $(form).serializeArray()[0]
+        );
+        super(data);
     }
+
+    get selector()
+    {
+        return (typeof id.toString() === 'string' && id !== Form.default())
+            ? `form[data-target="${this.id}"]`
+            : Form.default();
+    }
+
+    get id(){
+        return this.target;
+    };
 }
