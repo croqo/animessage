2413f18964d734bc172238002e3690a130a0b099
Eugene Croquette <croqo@outlook.com>
2020-10-16 12:11:44 +0300

    CU-90xj5z Form serializing implemented

--- src/js/form-jquery.js
+++ src/js/form-jquery.js
@@ -6,11 +6,12 @@ export default class Form extends Model
     static default(){return 'form'};
 
     constructor(form) {
-        let data = Object.assign(
-            $(form).data(),
-            $(form).serializeArray()[0]
+        super(
+            Object.assign(
+                $(form).data(),
+                Form.serialize(form)
+            )
         );
-        super(data);
     }
 
     get selector()
@@ -23,4 +24,30 @@ export default class Form extends Model
     get id(){
         return this.target;
     };
+
+    static serialize(form)
+    {
+        let array = $(form).serializeArray();
+        let result = [];
+        $.each(array, function (i){
+            console.log();
+            let item = array[i];
+            let value;
+            switch (item["name"]) {
+                case "loop":
+                    value = item["value"] === "on";
+                    break;
+                default:
+                    value = item["value"];
+            }
+            result[item["name"]] = value;
+        });
+        return result;
+    }
+
+    static async getForm(form) {
+        return new Form(form);
+    }
+
+
 }
