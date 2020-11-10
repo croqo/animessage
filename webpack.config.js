a7014ce251cd0a62d413e2d8de43cbb8cf885d0d
Eugene Croquette <croqo@outlook.com>
2020-11-10 11:31:31 +0200

    CU-a0vytc build successfully animationData bundle _lottie.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,88 @@
+const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+readLottieBase();
 
-let config = {
-  mode: 'development',
-  // mode: 'production',
-  entry: {
-    app : ['./src/app.js'],
-    base: ['./src/base.js']
-  },
-  output: {
-    path: path.resolve(__dirname,'animessage/inc')
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new webpack.ProgressPlugin({
-      percentBy: 'entries'
-    })
-  ],
+const
+    src = path.resolve(__dirname, 'src'),
+    config = new Promise(resolve => {
+  resolve({
+    mode: 'development',
+    // mode: 'production',
+    entry: {
+      // app : ['./src/app.js'],
+      base: [
+        path.join(src, '/base.js'),
+        path.join(src, '/base.json'),
+        path.join(src, '/_lottie.json')
+      ]
+    },
+    output: {
+      path: path.resolve(__dirname,'animessage/inc')
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new webpack.SourceMapDevToolPlugin({}),
+      new webpack.ProgressPlugin({
+        percentBy: 'entries'
+      })
+    ],
 
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          { loader: 'style-loader', options: { injectType: 'styleTag' } },
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.json$/,
-        loader: 'json5-loader',
-        type: 'javascript/auto'
-      }
-    ]
-  },
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            { loader: 'style-loader', options: { injectType: 'styleTag' } },
+            'css-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+    },
 
-  watchOptions: {
-    aggregateTimeout: 200,
-    poll: 1000
-  },
+    watchOptions: {
+      aggregateTimeout: 200,
+      poll: 1000
+    },
 
-  optimization: {
-    minimizer: [new TerserPlugin()]
-  }
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin({
+        // include: /\/src/,
+        // parallel:true
+      })]
+    }
+  });
+});
+
+readLottieBase().then(data =>{
+  writeLottieIndex(data).then(()=>{
+    module.exports = config;
+  })
+})
+
+function readLottieBase() {
+  return new Promise(resolve => {
+    const
+        dir = path.resolve(__dirname, 'src/lottie'),
+        files = fs.readdirSync(dir), filesCount = files.length;
+    let result = `"count":${filesCount}`, i=0;
+    files.forEach((fileName)=>{
+      let fileData = fs.readFileSync(path.resolve(dir,fileName),"utf8");
+      result = result + `, "${fileName}":${fileData}`;
+    });
+    resolve(`{${result}}`);
+  })
 }
-
-module.exports = config;
+function writeLottieIndex(data){
+  const
+      fsPromise = require('fs').promises,
+      indexPath = path.resolve(__dirname, "src/_lottie.json");
+  return fsPromise.writeFile(indexPath, data);
+}
 
