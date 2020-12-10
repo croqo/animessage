1550c9438f177901dd691882ac0d9e62250000fd
Eugene Croquette <croqo@outlook.com>
2020-12-10 22:57:39 +0200

    setup, unit containers templates

--- src/js/template.js
+++ src/js/template.js
@@ -3,7 +3,18 @@ export default class Template
     static appName(){return "aniMessage"}
     static appHtml(){
         return $(`#${Template.appName()}`).get(0)
-            ?? $(`<div id="${Template.appName()}"></div>`).appendTo("body")
+            ?? $(`<div id="${Template.appName()}"></div>`).appendTo("body").get(0)
     }
-
+    static setupHtml(id){
+        return (
+            $(Template.appHtml()).find(`div.setup.${id}`).get(0)
+            ?? $(`<div class="setup ${id}"></div>`).appendTo(Template.appHtml()).get(0)
+        )
+    }
+    static unitHtml(setup_html, unit_id){
+        return (
+            $(setup_html).find(`div.unit.${unit_id}`).get(0)
+            ?? $(`<div class="unit ${unit_id}"></div>`).appendTo(setup_html).get(0)
+        )
+    }
 }
