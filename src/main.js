34c34ba36556cb8c3c161b6dc56daead35a9163a
Eugene Croquette <croqo@outlook.com>
2020-12-10 23:00:32 +0200

    cleanup a little

--- src/main.js
+++ src/main.js
@@ -2,37 +2,29 @@ import "./style.css";
 import jquery from 'jquery';
 globalThis.jQuery
     = globalThis.$ = jquery;
-import {Howl} from 'howler'
-import lottie from 'lottie-web';
-import Unit from "./js/unit";
 import Setup from "./js/setup";
 import Template from "./js/template";
 
 let app = globalThis[Template.appName()] = {
-    html: $(`<div id="${Template.appName()}"></div>`)
+    config: Сonfig
 };
-
-setTimeout(()=>{
-    app.data = Сonfig;
-    console.log("Config ready");
-    setTimeout(()=>{
-        app.html = $(Template.appHtml()).appendTo("body");
-        console.log("app container injected to body");
-        $.when(
-            $.each(app.data, function (name, units){
-                let setup = new Setup(name);
-                setTimeout(()=>{
-                    setup.getContent(units);
-                });
-                app.data[name] = setup;
-            })
-        ).then(function (){
-            console.log(app.data);
-        });
-        setTimeout(()=>{
-            console.log("content ready?");
+$(document).ready(()=>{
+    app.html = Template.appHtml();
+    console.log("app container injected to body");
+    $.when(
+        $.each(app.config, function (name, units){
+            let setup = new Setup(name);
+            setTimeout(()=>{
+                setup.content=units;
+            });
+            app.config[name] = setup;
         })
+    ).then(function (){
+        console.log(app.config);
     });
+    setTimeout(()=>{
+        console.log("content ready?");
+    })
 })
 
 // function zFlip(element){
